01d7b6a308d0aca84d49a9b64bc84130da3d24d5
lucho1599 <74988201+lucho1599@users.noreply.github.com>
2021-02-08 23:12:22 -0500

    Add files via upload

---
+++ lista-prev.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 14:16:44 2020
+
+@author: luisv
+"""
+
+lista=["R1",5.5,6,True,"R2"]
+print(lista[1])
+print(lista[-1])
+print(lista[-5])
+del lista[4]
+print(lista)
+lista[1]="R3"
+print(lista)
