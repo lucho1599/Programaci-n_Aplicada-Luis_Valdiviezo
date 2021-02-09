67a08f5ff274c37d15722d4eb4ac3b2b5a01c3ff
lucho1599 <74988201+lucho1599@users.noreply.github.com>
2021-02-08 23:17:18 -0500

    Add files via upload

---
+++ lista-parte2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 14:32:52 2020
+
+@author: luisv
+"""
+
+dict1={"R1":"192.168.0.1",
+       "R2":"192.168.0.2",
+       "R3":"192.168.0.3"}
+print(dict1)
+print(dict1['R2'])
+dict1['R4']="192.168.0.4"
+dict1['S1']="192.168.1.3"
+print('R1' )
+
+
