816ff03e6994a6d1a63e04cce1d2aa31d75c2237
lucho1599 <74988201+lucho1599@users.noreply.github.com>
2021-02-08 22:59:20 -0500

    Add files via upload

---
+++ Cisco_Networking.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 14:21:39 2020
+
+@author: luisv
+"""
+
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+space=" "
+print(str1+space+str2+space+str3)
+print(str1+space+str2+space+str3+"\n")
+print(str1+str2+str3)
+print(str1,str2,str3)
+x=5
+x=str(x)
+x=float(x)
+x=bool(x)
+x=int(x)
