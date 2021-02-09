d2e413ca5bc26191c0c4c84eec080b376c1e0f44
lucho1599 <74988201+lucho1599@users.noreply.github.com>
2021-02-08 23:06:26 -0500

    Add files via upload

---
+++ tipos-de-datos.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 15:13:56 2020
+
+@author: luisv
+"""
+variable=type(8)
+print(variable,"\n"*2)
+print(type(8.5))
+print(type(8))
+print(type(False))
+nota1=45
+promedio=nota1+5
+print(promedio)
